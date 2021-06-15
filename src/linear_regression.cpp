6700e321c28d999e72256a93bddd7e382d501ad4
Martin Saied <martin@datia.app>
2021-06-14 21:41:02 -0300

    HABEMUS LINEAR REGRESSION

---
+++ src/linear_regression.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+//#include <chrono>
+#include <pybind11/pybind11.h>
+#include <iostream>
+#include <exception>
+#include "linear_regression.h"
+
+using namespace std;
+namespace py=pybind11;
+
+LinearRegression::LinearRegression()
+{
+}
+
+void LinearRegression::fit(Matrix X, Matrix y)
+{
+    // Aplicar LDLT para ver si va mas rapido usando cholesovbksy
+    _solucion = (X.transpose() * X).ldlt().solve(X.transpose() * y);
+}
+
+
+Matrix LinearRegression::predict(Matrix X)
+{
+    return X * _solucion;
+}
